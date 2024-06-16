42368245c0ef85b5bd6c6cd1a0e532c458e6cf7e
aakash-sharma25 <as024harma@gmail.com>
2024-06-16 15:29:23 +0530

    configaration file

---
+++ client/vite.config.js
@@ -0,0 +1,26 @@
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": "http://localhost:8080",
+    },
+  },
+});
+
+// import path from "path"
+// import react from "@vitejs/plugin-react"
+// import { defineConfig } from "vite"
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
